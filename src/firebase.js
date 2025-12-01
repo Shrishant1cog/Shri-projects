066f4a914e7b7279b112f34b32c2668154544bb6
Shrishanth <shrishantsandigawad1@gmail.com>
2025-12-02 00:59:17 +0530

    wire checkout to netlify razorpay function

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,31 @@
-// src/firebase.js
-import { initializeApp } from "firebase/app";
+
+import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // 🔴 REPLACE ALL VALUES BELOW with your real config from Firebase console
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyABCD123",
-  authDomain: "shristore-12345.firebaseapp.com",
-  projectId: "shristore-12345",
-  storageBucket: "shristore-12345.appspot.com",
-  messagingSenderId: "9876543210",
-  appId: "1:9876543210:web:xyz123",
+  apiKey: "AIzaSyBvTjs5mBvFW3J9ZrouI6juqnr8mY9FAi4",
+  authDomain: "shri-1313f.firebaseapp.com",
+  projectId: "shri-1313f",
+  storageBucket: "shri-1313f.firebasestorage.app",
+  messagingSenderId: "289024991612",
+  appId: "1:289024991612:web:6f8831a56518cfe00e84f9",
+  measurementId: "G-Z6Q5RSS1HX"
 };
 
-
-// ✅ Initialize app once
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // ✅ Export auth + Google provider
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
